580f783507863787c8b2f0dd407b4efb96e2b953
ninajigania <ninajigania@gmail.com>
2023-01-31 03:00:07 +0400

    update

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ nameArray.shift();
 console.log(nameArray , nameArray.lenght);
 
 
+
+let arr = [];
+for (let i = 0; i < 1000; i++) {
+    arr.push(i * i);
+}
+console.log(arr);
+
+
+for (let index = 0; index < 100; index++) {
+    
+    console.log("index");
+    
+}
+
+let item = [];
+while (item < 50) {
+    console.log("index");
+    item++;
+}
+
+
+do {
+    console.log("index");
+    item++;
+} while (item < 150 );
